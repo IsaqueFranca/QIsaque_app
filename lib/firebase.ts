8de1f02e1f850d8ffb544c345d9aeb6266636515
IsaqueFranca <isaquebpereira50@gmail.com>
2025-12-04 15:56:31 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,8 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Configuration uses environment variables defined in vite.config.ts
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD7zI-wYCzo8vjvmXYBvpHjiFOhEdf_bGE",
   authDomain: "qisaque-ea6cb.firebaseapp.com",
@@ -16,8 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
