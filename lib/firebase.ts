f5a2cbf2983f028d19f1e624f6dc89e4f19692bd
IsaqueFranca <isaquebpereira50@gmail.com>
2025-12-04 20:50:43 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,17 @@
+// @ts-ignore
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Configuration uses environment variables defined in vite.config.ts
 const firebaseConfig = {
-  apiKey: "AIzaSyD7zI-wYCzo8vjvmXYBvpHjiFOhEdf_bGE",
-  authDomain: "qisaque-ea6cb.firebaseapp.com",
+  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyD7zI-wYCzo8vjvmXYBvpHjiFOhEdf_bGE",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "qisaque-ea6cb.firebaseapp.com",
   databaseURL: "https://qisaque-ea6cb-default-rtdb.firebaseio.com",
-  projectId: "qisaque-ea6cb",
-  storageBucket: "qisaque-ea6cb.firebasestorage.app",
-  messagingSenderId: "844065251736",
-  appId: "1:844065251736:web:b53072999eb1e614558bed",
+  projectId: process.env.FIREBASE_PROJECT_ID || "qisaque-ea6cb",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "qisaque-ea6cb.firebasestorage.app",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "844065251736",
+  appId: process.env.FIREBASE_APP_ID || "1:844065251736:web:b53072999eb1e614558bed",
   measurementId: "G-088SBGNVPT"
 };
 
